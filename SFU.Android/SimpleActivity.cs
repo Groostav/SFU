e5280c91a94d560985ddb41ecb86597d4b51cdc8
Geoff Groos <geoff_groos@msn.com>
2013-05-05 17:05:03 -0700

    changed linking system
    - any new references to shared code require linking at the file level,
    rather than DLL-level

---
+++ SFU.Android/SimpleActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using SFU.Services;
+
+namespace SFU.Android
+{
+    [Activity(Label = "SFU.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class SimpleActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            //reach into the linked shared code to create a service!
+            var service = new SimpleService();
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            var button = FindViewById<Button>(Resource.Id.MyButton);
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", service.SimpleProp++); };
+        }
+    }
+}
+
---
+++ SFU.Services/SimpleService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFU.Services
+{
+    public class SimpleService
+    {
+        public SimpleService()
+        {
+        }
+
+        public int SimpleProp { get; set; }
+
+        public string anotherProp { get; set; }
+    }
+}
